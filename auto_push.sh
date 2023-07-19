f313b3ac6f5b3319fef13e24a4a0f2b15f4e8087
wocheon <wocheon07@gmail.com>
2023-07-19 13:46:43 +0900

    202307191346

--- auto_push.sh
+++ auto_push.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
-
-time=$(date '+%y%m%d%H%M')
+time=$(date '+%Y%m%d%H%M')
+branch=$(git branch | gawk '{print $2}')
+remote=$( git remote -v | grep push | gawk '{print $1}' )
 
 git add .
+
+if [ $? -eq 0 ]; then
+	echo -e "\E[42;37m'ADD : OKE[0m"
+else 
+	echo -e "\E[41;37mADD : FAIL\E[0m"
+	exit
+fi
+
 git commit -m "$time"
-git push origin main --force
+
+if [ $? -eq 0 ]; then
+	echo -e "\E[42;37m'COMMIT : OKE[0m"
+else 
+	echo -e "\E[41;37mCOMMIT : FAIL\E[0m"
+	exit
+fi
+
+git push $remote $branch --force
+
+
+if [ $? -eq 0 ]; then
+	echo -e "\E[42;37m'PUSH : OKE[0m"
+else 
+	echo -e "\E[41;37mPUSH : FAIL\E[0m"
+	exit
+fi
