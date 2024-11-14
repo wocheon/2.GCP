fb308737400494e765903a63a5f0201b5b3e6ee7
wocheon <82438284+wocheon@users.noreply.github.com>
2024-11-14 16:09:29 +0900

    Update 디스크내_전체디렉토리_사이즈확인_스크립트.sh

--- 2-5.Scripts/디스크내_전체디렉토리_사이즈확인_스크립트.sh
+++ 2-5.Scripts/디스크내_전체디렉토리_사이즈확인_스크립트.sh
@@ -10,4 +10,4 @@ du -sh --exclude=/share --exclude=/proc /* | sort -rh
 
 # 가장 사이즈가 큰 10개 파일 목록 출력 ( /share , /proc 디렉토리는 검색대상에서 제외)
 echo "# List the top 10 largest files (exclude /share, /proc)"
-find / -path /share -prune -o -path /proc -prune -o -type f -exec du -h {} + | sort -rh | head -n 1
+find / -path /share -prune -o -path /proc -prune -o -type f -exec du -h {} + | sort -rh | head -n 10
